b8b91bbb45df820cac02addcc4570f02357e0ea7
danil <jardus@bk.ru>
2022-10-29 15:23:50 +0300

    HomeWork_6

---
+++ Задача 1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Write($"Введи число М(количество чисел): ");
+int M = int.Parse(Console.ReadLine());
+int[] Massiv = new int[M];
+
+void Numbers(int M)
+{
+for (int i = 0; i < M; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    Massiv[i] = int.Parse(Console.ReadLine());
+  }
+}
+int Result(int[] Massiv)
+{
+  int count = 0;
+  for (int i = 0; i < Massiv.Length; i++)
+  {
+    if(Massiv[i] > 0 ) count++; 
+  }
+  return count;
+}
+
+Numbers(M);
+Console.WriteLine($"Количество чисел больше 0: {Result(Massiv)} ");
---
+++ Задача 2/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+double[,] Coordinate = new double[2, 2];
+double[] CrossPoint = new double[2];
+
+void InputCoordinate()
+{
+  for (int i = 0; i < Coordinate.GetLength(0); i++)
+  {
+    Console.Write($"Введите координаты для {i+1}-го уравнения (y = k * x + b):\n");
+    for (int j = 0; j < Coordinate.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите коэффициент k: ");
+      else Console.Write($"Введите коэффициент b: ");
+      Coordinate[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Decision(double[,] Coordinate)
+{
+  CrossPoint[0] = (Coordinate[1,1] - Coordinate[0,1]) / (Coordinate[0,0] - Coordinate[1,0]);
+  CrossPoint[1] = CrossPoint[0] * Coordinate[0,0] + Coordinate[0,1];
+  return CrossPoint;
+}
+
+void OutputResponse(double[,] Coordinate)
+{
+  if (Coordinate[0,0] == Coordinate[1,0] && Coordinate[0,1] == Coordinate[1,1]) 
+  {
+    Console.Write($"\nПрямые совпадают");
+  }
+  else if (Coordinate[0,0] == Coordinate[1,0] && Coordinate[0,1] != Coordinate[1,1]) 
+  {
+    Console.Write($"\nПрямые параллельны");
+  }
+  else 
+  {
+    Decision(Coordinate);
+    Console.Write($"\nТочка пересечения прямых: ({CrossPoint[0]}, {CrossPoint[1]})");
+  }
+}
+
+InputCoordinate();
+OutputResponse(Coordinate);
